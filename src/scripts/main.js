76c5e8789496e881656e0dfcdc3cf8cdb5ab39e7
Maksym Korchynskyi <maksym.24.09.2003@gmail.com>
2024-12-22 18:29:00 +0200

    fix the mistakes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,16 +3,16 @@
 const wall = document.querySelector('.wall');
 const spider = wall.querySelector('.spider');
 
-const topMargin = wall.offsetTop + wall.clientTop + spider.clientHeight / 2;
-const leftMargin = wall.offsetLeft + wall.clientLeft + spider.clientWidth / 2;
-
 const topMax = wall.clientHeight - spider.clientHeight;
 const leftMax = wall.clientWidth - spider.clientWidth;
 
+const offsetTop = wall.offsetTop + wall.clientTop + spider.clientHeight / 2;
+const offsetLeft = wall.offsetLeft + wall.clientLeft + spider.clientWidth / 2;
+
 document.addEventListener('click', (e) => {
   if (e.target.closest('.wall')) {
-    let y = e.clientY - topMargin;
-    let x = e.clientX - leftMargin;
+    let y = e.clientY - offsetTop;
+    let x = e.clientX - offsetLeft;
 
     y = Math.max(y, 0);
     y = Math.min(y, topMax);
